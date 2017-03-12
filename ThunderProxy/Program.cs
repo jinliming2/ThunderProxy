bc24fd08a278135d707afcf37ac48fd63f7fa181
Liming Jin <jinliming2@gmail.com>
2017-03-12 10:24:03 +0800

    Log.🎈

--- ThunderProxy/Program.cs
+++ ThunderProxy/Program.cs
@@ -17,14 +17,17 @@ namespace ThunderProxy {
             var program = new Program();
             program.LoadConfig();
             if(args.Length < 1) {
-                Console.WriteLine("按回车键退出...");
+                log("按回车键退出...");
                 Console.ReadLine();
                 return;
             }
+            Console.Title = "Thunder Proxy - 按 Ctrl + C 退出";
             IEnumerator ie = program.Start(args[0]).GetEnumerator();
             ie.MoveNext();
             int port = (int)ie.Current;
+            log("开始监听本地" + port + "端口...");
             Process.Start(program.command, Regex.Replace(args[0], @":\d+/", ":" + port + "/"));
+            log("已执行启动播放器命令...");
             ie.MoveNext();
             Console.ReadLine();
         }
@@ -38,7 +41,7 @@ namespace ThunderProxy {
 <Config>
     <Command><![CDATA[C:\Program Files\VideoLAN\VLC\vlc.exe]]></Command>
 </Config>");
-                Console.WriteLine("已生成默认配置文件！");
+                log("已生成默认配置文件！");
             }
             XDocument xml = XDocument.Load(CONFIG_FILE);
             command = xml.Element("Config").Element("Command").Value;
@@ -53,8 +56,9 @@ namespace ThunderProxy {
             yield return (tcpListener.LocalEndpoint as IPEndPoint).Port;
             while(true) {
                 try {
+                    log("等待连接...");
                     TcpClient client = tcpListener.AcceptTcpClient();
-                    int p = int.Parse(Regex.Match(address, @":(\d+)/").Groups[1].Value);
+                    log("已接受连接...");
                     TcpClient server = new TcpClient("127.0.0.1", int.Parse(Regex.Match(address, @":(\d+)/").Groups[1].Value));
                     NetworkStream c_stream = client.GetStream();
                     NetworkStream s_stream = server.GetStream();
@@ -69,7 +73,7 @@ namespace ThunderProxy {
                             //    flag = false;
                             //    header += "Connection: close\r\n";
                             //} else {
-                                header += line + "\r\n";
+                            header += line + "\r\n";
                             //}
                         }
                         //if(flag) {
@@ -84,18 +88,42 @@ namespace ThunderProxy {
                             c_stream.Write(buffer, 0, length);
                         }
                         c_stream.Flush();
+                    } catch(IOException e) {
+                        warn(e.Message);
+                        c_reader.Dispose();
+                        s_writer.Dispose();
+                        c_stream.Dispose();
+                        s_stream.Dispose();
                     } catch(Exception e) {
-                        Console.WriteLine(e.Message);
+                        error(e.Message + "\n" + e.StackTrace);
                         c_reader.Dispose();
                         s_writer.Dispose();
                         c_stream.Dispose();
                         s_stream.Dispose();
                     }
                 } catch(Exception e)  {
-                    Console.WriteLine(e.Message);
+                    error(e.Message + "\n" + e.StackTrace);
                     break;
                 }
             }
+        }
+
+        static void log(string message) {
+            Console.Out.WriteLine(DateTime.Now.ToString() + " " + message);
+        }
+
+        static void error(string message) {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine(DateTime.Now.ToString() + " " + message);
+            Console.ResetColor();
+        }
+
+        static void warn(string message) {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.Out.WriteLine(DateTime.Now.ToString() + " " + message);
+            Console.ResetColor();
         }
     }
 }
